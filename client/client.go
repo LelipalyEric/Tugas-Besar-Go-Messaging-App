f6a6e5abfbcf698cbb76e9e90edca536bf50e5b2
LelipalyEric <lelipalyeric@gmail.com>
2026-06-14 18:05:26 +0700

    adding room feature

--- client/client.go
+++ client/client.go
@@ -8,9 +8,13 @@ import (
 	"strings"
 )
 
+var (
+	isInRoom bool
+)
+
 func main() {
 
-	// Ngehubungin si client ke seerver
+	// Ngehubungin si client ke server
 	conn, err := net.Dial("tcp", ":9090")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot connect to server!")
@@ -49,39 +53,158 @@ func main() {
 		}
 	}
 
-	fmt.Println("Start typing your messages")
+	//Input room
+	fmt.Println("What do you wanna do?")
+	fmt.Println("* /create to create room")
+	fmt.Println("* /join to join a room")
+	fmt.Println("* /list to list all available room")
+	fmt.Println("* /exit to close the program")
+	commandInput := ""
 
-	go handleIncomingMessage(conn)
+	isInRoom = false
 
 	for {
-		//Input messages
-		message, err := localReader.ReadString('\n')
+		commandInput, err = localReader.ReadString('\n')
 
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Cannot read the message!")
+			fmt.Fprintf(os.Stderr, "Cannot read the command!")
 			continue
 		}
 
-		if message == "/exit" {
+		commandInput = strings.TrimSpace(commandInput)
+
+		if commandInput == "/create" {
+			fmt.Fprint(conn, "/create\n")
+			fmt.Println("Choose room name")
+
+			for {
+				commandInput, err = localReader.ReadString('\n')
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "disconnected during room creating session.")
+					return
+				}
+
+				conn.Write([]byte(commandInput)) // fmt.Fprint(conn, message)
+				status, err := connReader.ReadString('\n')
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "disconnected during room creating session.")
+					return
+				}
+
+				status = strings.TrimSpace(status)
+
+				if status == "APPROVED" {
+					fmt.Println("Room is Created")
+					break
+				} else {
+					fmt.Println("ERROR DUPLICATE ROOM NAME, TRY AGAIN")
+				}
+			}
+
+		} else if commandInput == "/join" {
+			fmt.Fprint(conn, "/join\n")
+			fmt.Println("Choose room you wanna join")
+
+			for {
+				commandInput, err = localReader.ReadString('\n')
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Server disconnected during room joining session.")
+					return
+				}
+
+				conn.Write([]byte(commandInput)) // fmt.Fprint(conn, message)
+				status, err := connReader.ReadString('\n')
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "disconnected during room creating session.")
+					return
+				}
+
+				status = strings.TrimSpace(status)
+
+				if status == "APPROVED" {
+					fmt.Println("Successfully Joined The Room")
+					isInRoom = true
+					break
+				} else {
+					fmt.Println("ERROR INVALID ROOM NAME")
+				}
+			}
+
+			fmt.Println("Start typing your messages")
+
+			go handleIncomingMessage(conn)
+
+			for {
+				//Input messages
+				message, err := localReader.ReadString('\n')
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Cannot read the message!")
+					continue
+				}
+
+				if strings.TrimSpace(message) == "/exit" {
+					fmt.Println("Exiting Room")
+					fmt.Fprint(conn, "/exit\n")
+					isInRoom = false
+					break
+				}
+
+				conn.Write([]byte(message)) // fmt.Fprint(conn, message)
+
+			}
+
+		} else if commandInput == "/list" {
+			fmt.Fprint(conn, "/list\n")
+			res := ""
+
+			for {
+				listRoom, err := connReader.ReadString('\n')
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Server disconnected during room joining session.")
+					return
+				}
+
+				listRoom = strings.TrimSpace(listRoom)
+
+				if listRoom == "END" {
+					break
+				}
+
+				res += listRoom + "\n"
+
+			}
+
+			fmt.Print(res)
+
+		} else if commandInput == "/exit" {
 			fmt.Fprint(conn, "/exit\n")
 			break
+		} else {
+			fmt.Println("Wrong Command Input")
 		}
-
-		conn.Write([]byte(message)) // fmt.Fprint(conn, message)
-
 	}
 }
 
 func handleIncomingMessage(conn net.Conn) {
 	connReader := bufio.NewReader(conn)
 
-	for {
+	for isInRoom {
+		if !isInRoom {
+			return
+		}
+
 		incoming, err := connReader.ReadString('\n')
 		if err != nil {
 			os.Exit(0)
+			return
 		} else {
+			//SAKTI ANJIR
+			fmt.Print("\r\033[K")
 			fmt.Print(incoming)
 		}
 	}
-
 }
--- server/server.go
+++ server/server.go
@@ -9,10 +9,18 @@ import (
 	"sync"
 )
 
+type client struct {
+	username string
+	room     string
+}
+
 var (
-	clients     = make(map[net.Conn]string)
+	clients = make(map[net.Conn]client)
+	// variable usernameMap adalah map / set untuk melihat apakah username sudah dipakai atau tidak
 	usernameMap = make(map[string]bool)
-	mtx         sync.Mutex
+	// variable room adalah map yang memetakan string nama room ke int jumlah client didalamnya
+	rooms = make(map[string]int)
+	mtx   sync.Mutex
 )
 
 func main() {
@@ -46,6 +54,21 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
+
+	defer func() {
+		mtx.Lock()
+		defer mtx.Unlock()
+
+		c := clients[conn]
+
+		if c.room != "" {
+			rooms[c.room]--
+		}
+
+		delete(clients, conn)
+		delete(usernameMap, strings.ToLower(c.username))
+	}()
+
 	reader := bufio.NewReader(conn)
 
 	usernameInput := ""
@@ -69,56 +92,191 @@ func handleClient(conn net.Conn) {
 				fmt.Fprint(conn, "REJECTED\n")
 			} else {
 				// keep username not lowerName
-				clients[conn] = username
+				tempC := client{
+					username: username,
+					room:     "",
+				}
+
+				clients[conn] = tempC
 				usernameMap[lowerName] = true
 				mtx.Unlock()
 
 				fmt.Fprint(conn, "APPROVED\n")
 				fmt.Println("Username has been recieved!")
 
-				// shows username not lowercased
-				joinMsg := fmt.Sprintf("%s has joined\n", username)
-				broadcast(joinMsg, conn)
 				break
 			}
 		}
 	}
 
+	commandInput := ""
+	roomInput := ""
 	message := ""
-
 	for {
-		message, err = reader.ReadString('\n')
+		commandInput, err = reader.ReadString('\n')
+
 		if err != nil {
-			exitMsg := fmt.Sprintf("%s has left the groupchat unexpectedly\n", username)
-			broadcast(exitMsg, conn)
+			fmt.Fprintf(os.Stderr, "Failed to read room input!")
 			break
+
 		} else {
-			fmt.Println("The message has been received!")
+			commandInput = strings.TrimSpace(commandInput)
+
+			if commandInput == "/create" {
+
+				for {
+					roomInput, err = reader.ReadString('\n')
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Failed to read room input name!")
+						break
+					}
+
+					if createRoom(roomInput) {
+						fmt.Fprint(conn, "APPROVED\n")
+						break
+					} else {
+						fmt.Fprint(conn, "REJECTED\n")
+					}
+				}
+
+			} else if commandInput == "/join" {
+
+				for {
+					roomInput, err = reader.ReadString('\n')
+
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Failed to read room input name!")
+					}
+
+					if joinRoom(conn, roomInput) {
+
+						fmt.Fprint(conn, "APPROVED\n")
+						joinMsg := fmt.Sprintf("%s has joined\n", username)
+						broadcast(joinMsg, conn)
+
+						for {
+							message, err = reader.ReadString('\n')
+							if err != nil {
+								exitMsg := fmt.Sprintf("%s has left the room unexpectedly\n", username)
+								broadcast(exitMsg, conn)
+								leaveRoom(conn)
+								break
+							}
+
+							if strings.TrimSpace(message) == "/exit" {
+								fmt.Printf("%s left room", username)
+								exitMsg := fmt.Sprintf("%s has left the room\n", username)
+								broadcast(exitMsg, conn)
+								leaveRoom(conn)
+								break
+							}
+
+							formatM := fmt.Sprintf("%s: %s", username, message)
+							broadcast(formatM, conn)
+						}
+						break
+					} else {
+						fmt.Fprint(conn, "REJECTED\n")
+					}
+				}
+
+			} else if commandInput == "/list" {
+				listRooms(conn)
+			} else if commandInput == "/exit" {
+				break
+			} else {
+				fmt.Fprintf(conn, "CREATE OR JOIN A ROOM FIRST")
+			}
 		}
 
-		if strings.TrimSpace(message) == "/exit" {
-			fmt.Printf("%s left groupchat", username)
-			exitMsg := fmt.Sprintf("%s has left the groupchat\n", username)
-			broadcast(exitMsg, conn)
-			break
-		}
-
-		formatM := fmt.Sprintf("%s: %s", username, message)
-		broadcast(formatM, conn)
 	}
-
-	mtx.Lock()
-	delete(clients, conn)
-	delete(usernameMap, strings.ToLower(username))
-	mtx.Unlock()
 }
 
 func broadcast(message string, sender net.Conn) {
 	mtx.Lock()
 	defer mtx.Unlock()
+
+	senderRoom := clients[sender].room
+
 	for conn := range clients {
 		if conn != sender {
-			fmt.Fprint(conn, message)
+			room := clients[conn].room
+
+			if room == senderRoom {
+				fmt.Fprint(conn, message)
+			}
 		}
 	}
+}
+
+func joinRoom(conn net.Conn, roomName string) bool {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	roomName = strings.ToLower(strings.TrimSpace(roomName))
+
+	// exist kalau key udah ada
+	_, exists := rooms[roomName]
+
+	if !exists {
+		return false
+	}
+	c := clients[conn]
+	c.room = roomName
+	clients[conn] = c
+
+	rooms[roomName]++
+
+	return true
+}
+
+func createRoom(roomName string) bool {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	roomName = strings.ToLower(strings.TrimSpace(roomName))
+	_, exists := rooms[roomName]
+
+	if exists {
+		return false
+	}
+
+	rooms[roomName] = 0
+	return true
+}
+
+func listRooms(conn net.Conn) {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	if len(rooms) == 0 {
+		fmt.Fprint(conn, "No rooms exists\n")
+		return
+	}
+
+	for roomName, count := range rooms {
+		fmt.Fprintf(conn, "- %s (%d users)\n", roomName, count)
+	}
+
+	fmt.Fprint(conn, "END\n")
+}
+
+func leaveRoom(conn net.Conn) {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	c := clients[conn]
+
+	if c.room == "" {
+		return
+	}
+
+	rooms[c.room]--
+
+	if rooms[c.room] <= 0 {
+		delete(rooms, c.room)
+	}
+
+	c.room = ""
+	clients[conn] = c
 }
